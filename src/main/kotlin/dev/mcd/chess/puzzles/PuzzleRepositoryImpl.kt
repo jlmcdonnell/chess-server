a7bf1a7fc25aa3ff6aa869c1bfbca46fcc88ebfc
John McDonnell <john+git@mcd.dev>
2023-06-26 22:16:05 +0100

    Move puzzle rating init to transaction

--- src/main/kotlin/dev/mcd/chess/puzzles/PuzzleRepositoryImpl.kt
+++ src/main/kotlin/dev/mcd/chess/puzzles/PuzzleRepositoryImpl.kt
@@ -1,31 +1,25 @@
 package dev.mcd.chess.puzzles
 
 import dev.mcd.chess.puzzles.db.Puzzles
+import dev.mcd.chess.puzzles.db.maxRatedPuzzle
+import dev.mcd.chess.puzzles.db.minRatedPuzzle
 import dev.mcd.chess.puzzles.db.toPuzzle
-import kotlinx.coroutines.runBlocking
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Random
+import org.jetbrains.exposed.sql.andWhere
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import kotlin.math.max
 import kotlin.math.min
 
 class PuzzleRepositoryImpl : PuzzleRepository {
 
-    private val allPuzzleRange: IntRange
+    private lateinit var allPuzzleRange: IntRange
 
     init {
-        val minRange = Puzzles.selectAll()
-            .orderBy(Puzzles.rating, SortOrder.DESC)
-            .limit(1)
-            .first()
-            .toPuzzle().rating
-
-        val maxRange = Puzzles.selectAll()
-            .orderBy(Puzzles.rating, SortOrder.ASC)
-            .limit(1)
-            .first()
-            .toPuzzle().rating
-
-        allPuzzleRange = minRange..maxRange
+        transaction {
+            allPuzzleRange = Puzzles.minRatedPuzzle()..Puzzles.maxRatedPuzzle()
+        }
     }
 
     override suspend fun getRandomPuzzle(ratingMin: Int, ratingMax: Int): Puzzle {
--- src/main/kotlin/dev/mcd/chess/puzzles/db/Puzzles.kt
+++ src/main/kotlin/dev/mcd/chess/puzzles/db/Puzzles.kt
@@ -3,6 +3,8 @@ package dev.mcd.chess.puzzles.db
 import dev.mcd.chess.puzzles.Puzzle
 import org.jetbrains.exposed.dao.id.IdTable
 import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.selectAll
 
 object Puzzles : IdTable<String>() {
     override val id = varchar("puzzleId", 255).entityId()
@@ -12,6 +14,20 @@ object Puzzles : IdTable<String>() {
     val themes = text("themes")
 }
 
+fun Puzzles.maxRatedPuzzle(): Int {
+    return Puzzles.selectAll()
+        .orderBy(rating, SortOrder.DESC)
+        .limit(1)
+        .first()[rating]
+}
+
+fun Puzzles.minRatedPuzzle(): Int {
+    return Puzzles.selectAll()
+        .orderBy(rating, SortOrder.ASC)
+        .limit(1)
+        .first()[rating]
+}
+
 fun ResultRow.toPuzzle() = Puzzle(
     puzzleId = this[Puzzles.id].value,
     fen = this[Puzzles.fen],
@@ -19,3 +35,4 @@ fun ResultRow.toPuzzle() = Puzzle(
     rating = this[Puzzles.rating],
     themes = this[Puzzles.themes].split(' '),
 )
+
